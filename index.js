d90042229b13e4a4c38726aa11763b287864cb5c
Alejandro Hernandez <alejandro.hernandez@yalochat.com>
2022-01-26 16:37:20 -0600

    Creando un servidor de Apollo

---
+++ index.js
@@ -0,0 +1,9 @@
+const { ApolloError, gql } = require('apollo-server')
+
+// servidor
+const server = new ApolloServer()
+
+// Arrancar el servidor
+server.listen().then(({url}) => {
+    console.log(`Servidor listo en la URL ${url}`);
+})
