2f981b6f4fea9ec1f45ff0bf7d405e903cf6a9a2
Alejandro Hernandez <alejandro.hernandez@yalochat.com>
2022-01-26 17:33:57 -0600

    Agregando types y resolvers en el codigo

--- index.js
+++ index.js
@@ -1,7 +1,50 @@
-const { ApolloError, gql } = require('apollo-server')
+const { ApolloServer, gql } = require('apollo-server')
+
+// Schema
+const typeDefs = gql`
+
+    type Curso {
+        titulo: String
+        tecnologia: String
+    }
+
+    type Query {
+        obtenerCursos : [Curso]
+    }
+
+`
+
+const cursos = [
+    {
+        titulo: 'JavaScript Moderno Guía Definitiva Construye +10 Proyectos',
+        tecnologia: 'JavaScript ES6',
+    },
+    {
+        titulo: 'React – La Guía Completa: Hooks Context Redux MERN +15 Apps',
+        tecnologia: 'React',
+    },
+    {
+        titulo: 'Node.js – Bootcamp Desarrollo Web inc. MVC y REST API’s',
+        tecnologia: 'Node.js'
+    }, 
+    {
+        titulo: 'ReactJS Avanzado – FullStack React GraphQL y Apollo',
+        tecnologia: 'React'
+    }
+]
+
+// Resolvers
+const resolvers = {
+    Query: {
+        obtenerCursos: () => cursos
+    }
+}
 
 // servidor
-const server = new ApolloServer()
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
 
 // Arrancar el servidor
 server.listen().then(({url}) => {
